c467ff0c2836ed4598ed4b793e33b2c2217a6c38
Hajtosek <hajtosek@gmail.com>
2015-05-23 13:36:37 +0200

    Finished

--- public/javascripts/hello.js
+++ public/javascripts/hello.js
@@ -7,13 +7,30 @@ function init() {
     }
     if(data.length > 0){
         for (var i = 0; i < data.length; i++) {
-            document.getElementById("mainContainer").appendChild(createOption(data[i].name))
+            document.getElementById("mainContainer").appendChild(createOption(data[i].name));
+
+            var table = $("<table>")
+                .addClass("table table-stripped")
+                .css({
+                    width: "100%",
+                    textAlign: "center"
+                });
+
+            for(var rows = 0; rows < data[i].content.length-1; rows++){
+                var row = $("<tr>")
+                    .append($("<td>").html(data[i].content[rows].hour))
+                    .append($("<td>").html(data[i].content[rows].who))
+                    .append($("<td>").html(data[i].content[rows].age));
+                $(table).append(row);
+            }
+            console.log(table);
+            parsed.push(table)
         }
 
         document.getElementById("mainContainer").addEventListener("change", function (event) {
-            document.getElementById("data").innerHTML = data[event.target.selectedIndex].content
+            $("#data").empty().append(parsed[event.target.selectedIndex])
         });
-        document.getElementById("data").innerHTML = data[0].content
+        $("#data").empty().append(parsed[0])
     } else {
         document.getElementById("mainContainer").style.display = "none";
         document.writeln("Brak rekordów do wyświetlenia")
